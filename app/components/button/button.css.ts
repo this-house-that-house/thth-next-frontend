9abc25688fb2bdcda421a9de3d7b6524131ee0b7
c9u11 <tjrdud6412@naver.com>
2024-09-28 02:37:38 +0900

    💄 button component recipe로 변경

--- app/components/button/button.css.ts
+++ app/components/button/button.css.ts
@@ -1,17 +1,30 @@
-import { style } from "@vanilla-extract/css";
 import { vars } from "../theme.css";
+import { recipe } from "@vanilla-extract/recipes";
 
-export const button = style({
-  display: "flex",
-  padding: "17px",
-  justifyContent: "center",
-  alignItems: "center",
-  border: "none",
-  borderRadius: vars.borderRadius,
-  color: vars.color.gray[50],
-  backgroundColor: vars.color.primary[100],
-  ":disabled": {
-    backgroundColor: vars.color.gray[200],
-    color: vars.color.gray[500],
+export const button = recipe({
+  base: {
+    display: "flex",
+    padding: "17px",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "none",
+    borderRadius: vars.borderRadius,
+    color: vars.color.gray[50],
+    ":disabled": {
+      backgroundColor: vars.color.gray[200],
+      color: vars.color.gray[500],
+    },
+  },
+  variants: {
+    color: {
+      primary: { backgroundColor: vars.color.primary[100] },
+      secondary: { backgroundColor: vars.color.gray[200] },
+      yellow: { backgroundColor: vars.color.yellow },
+      orange: { backgroundColor: vars.color.orange },
+      green: { backgroundColor: vars.color.green },
+    },
+  },
+  defaultVariants: {
+    color: "primary",
   },
 });
