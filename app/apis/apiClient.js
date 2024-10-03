6968af53f881a9d01e8764638482fc12be972ba8
c9u11 <tjrdud6412@naver.com>
2024-10-03 15:37:23 +0900

    ✨ api client

---
+++ app/apis/apiClient.js
@@ -0,0 +1,30 @@
+'use client'
+
+import axios from 'axios'
+import { TOKEN_KEY_NAME } from '/app/constants/auth'
+
+const apiClient = axios.create()
+
+apiClient.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL
+apiClient.defaults.headers.common['Content-Type'] = 'application/json'
+if (typeof window !== 'undefined') {
+  apiClient.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem(TOKEN_KEY_NAME)}`
+}
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem(TOKEN_KEY_NAME)
+
+    config.headers['Authorization'] = `Bearer ${token}`
+
+    return config
+  },
+  (error) => {
+    delete apiClient.defaults.headers.common.Authorization
+    window.location.href = '/login'
+
+    return Promise.reject(error)
+  }
+)
+
+export default apiClient
