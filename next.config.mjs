1ebcadd730ae5ec33447fbdda1bb232005719a3b
c9u11 <tjrdud6412@naver.com>
2024-10-03 13:37:09 +0900

    👷 github.io 업데이트를 위한 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 const withVanillaExtract = createVanillaExtractPlugin();
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -11,5 +12,10 @@ const nextConfig = {
 
     return config;
   },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? 'https://this-house-that-house.github.io/thth-next-frontend/' : '',
 };
 export default withVanillaExtract(nextConfig);
