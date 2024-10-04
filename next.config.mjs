2477e77b2702c10479b47cba5101c4457b44d4b8
c9u11 <tjrdud6412@naver.com>
2024-10-04 15:49:56 +0900

    👷 정적파일 요청 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? './' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://this-house-that-house.github.io/thth-next-frontend/' : '',
   reactStrictMode: false,
 };
 
