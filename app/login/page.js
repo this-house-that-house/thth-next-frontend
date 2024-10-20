2273d846c669c4f4042e78a2b592fb2b3f531e72
c9u11 <tjrdud6412@naver.com>
2024-10-21 01:25:15 +0900

    💄 login page

---
+++ app/login/page.js
@@ -0,0 +1,50 @@
+
+import Link from 'next/link';
+import * as Styles from '../components/index.css';
+import RedHouseIcon from '../svgs/red-house.svg';
+import YellowHouseIcon from '../svgs/yellow-house.svg';
+
+export default function () {
+  return (
+    <div className={Styles.center} style={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+      padding: '100px 16px 0px'
+    }}>
+      <span className={Styles.logoText} style={{
+        fontSize: '34px',
+        marginBottom: 36
+      }}>
+        이집저집
+      </span>
+      <div style={{ marginBottom: 48 }}>
+        <RedHouseIcon style={{ marginRight: 9 }} />
+        <YellowHouseIcon />
+      </div>
+      <span style={{
+        ...Styles.vars.font.headingSB20,
+        textAlign: 'center',
+        marginBottom: 85
+      }}>
+        대학가 자취방, <br />이제 똑똑하게 비교하며 고르세요!
+      </span>
+      <img src="/imgs/kakao_login_large_wide.png" style={{
+        width: '100%',
+        marginBottom: 57
+      }} />
+      <Link href='/home' style={{
+        ...Styles.vars.font.bodyM16,
+        color: '#989898',
+        textDecoration: 'none',
+        paddingBottom: 2,
+        borderBottom: '1px solid #989898'
+      }}>
+        로그인 없이 둘러보기
+      </Link>
+    </div>
+  )
+}
