ca79a6fc0921fb4a8c639841f0760531c725ae58
c9u11 <tjrdud6412@naver.com>
2024-11-03 23:35:24 +0900

    ✨ 카카오맵 연동

---
+++ app/map/layout.js
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+import { container } from "../components/index.css"
+
+export default function ({ children }) {
+  return (
+    <>
+      <div className={container}>
+        {children}
+      </div>
+      <Nav />
+    </>
+  )
+}
