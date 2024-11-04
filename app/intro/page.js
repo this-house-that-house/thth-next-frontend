d7be2567fb0b60167e90ca2a433a11ee87e83864
c9u11 <tjrdud6412@naver.com>
2024-11-04 17:31:03 +0900

    💄 intro

--- app/intro/page.js
+++ app/intro/page.js
@@ -3,14 +3,13 @@
 import Button from "../components/Button";
 import Carousel from "../components/Carousel";
 import { useState } from "react";
-import { introMain, carouselInner } from "../components/index.css";
 
 export default function () {
   const [activeIndex, setActiveIndex] = useState(0);
   return (
-    <div className={introMain}>
+    <div>
       <Carousel activeIndex={activeIndex} setActiveIndex={setActiveIndex}>
-        <div className={carouselInner}>
+        <div>
           <img src="https://via.placeholder.com/150" />
         </div>
         <img src="https://via.placeholder.com/200" />
