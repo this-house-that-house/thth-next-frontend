2ecd1211457c612fc8710cb796c020f0bedf6828
c9u11 <tjrdud6412@naver.com>
2024-11-05 22:44:18 +0900

    ✨ 지도에 현재 위치 표시

--- app/map/page.js
+++ app/map/page.js
@@ -22,43 +22,25 @@ export default function () {
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
 
-        // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
         if (navigator.geolocation) {
-
-          // GeoLocation을 이용해서 접속 위치를 얻어옵니다
           navigator.geolocation.getCurrentPosition(function (position) {
-
             var lat = position.coords.latitude, // 위도
               lon = position.coords.longitude; // 경도
 
-            var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
-              message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+            var locPosition = new kakao.maps.LatLng(lat, lon);
 
-            // 마커와 인포윈도우를 표시합니다
-            displayMarker(locPosition, message);
+            displayMarker(locPosition);
 
-            // 지도에 마커와 인포윈도우를 표시하는 함수입니다
-            function displayMarker(locPosition, message) {
-
-              // 마커를 생성합니다
-              var marker = new kakao.maps.Marker({
+            function displayMarker(locPosition) {
+              const marker = new kakao.maps.Marker({
                 map: map,
-                position: locPosition
+                position: locPosition,
+                image: new kakao.maps.MarkerImage(
+                  '/imgs/my-marker.png',
+                  new kakao.maps.Size(40, 40),
+                  { offset: new kakao.maps.Point(20, 20) }
+                ),
               });
-
-              var iwContent = message, // 인포윈도우에 표시할 내용
-                iwRemoveable = true;
-
-              // 인포윈도우를 생성합니다
-              var infowindow = new kakao.maps.InfoWindow({
-                content: iwContent,
-                removable: iwRemoveable
-              });
-
-              // 인포윈도우를 마커위에 표시합니다 
-              infowindow.open(map, marker);
-
-              // 지도 중심좌표를 접속위치로 변경합니다
               map.setCenter(locPosition);
             }
           });
